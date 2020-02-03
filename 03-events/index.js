28ba28ba8c73831d1a061a27c2e23954d75967da
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-02-02 22:44:04 -0300

    Entendendo a diferença de Eventos e Promise

--- 03-events/index.js
+++ 03-events/index.js
@@ -10,6 +10,20 @@ meuEmissor.on(nomeEvento, function(click) {
 });
 
 const stdin = process.openStdin();
-stdin.addListener("data", function(value) {
-	console.log(`você digitou: ${value.toString().trim()}`);
+function main() {
+	return new Promise(function(resolve, reject) {
+		stdin.addListener("data", function(value) {
+			// console.log(`você digitou: ${value.toString().trim()}`);
+			return resolve(value);
+		});
+	});
+}
+/*
+	Usando Promise o Resultado so é mostrado no console.log somente uma unica vez,
+	pois esse é o comportamento de uma promise, ser realisado somente uma vez...
+
+	e os eventos são usado para ações contínuas.
+*/
+main().then(function(result) {
+	console.log("resultado ", result.toString());
 });
