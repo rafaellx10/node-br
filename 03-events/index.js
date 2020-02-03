f2b2cbfdc726ddcb9ab77ea162343ce00fa55db2
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-02-02 21:59:51 -0300

    usando EventMitter para fazer um contador

---
+++ 03-events/index.js
@@ -0,0 +1,18 @@
+const EventEmitter = require("events");
+
+class MeuEmissor extends EventEmitter {}
+
+const meuEmissor = new MeuEmissor();
+const nomeEvento = "usuario:click";
+
+meuEmissor.on(nomeEvento, function(click) {
+	console.log("um usuário clicou ", click);
+});
+
+meuEmissor.emit(nomeEvento, "na barra de rolagem");
+meuEmissor.emit(nomeEvento, "no ok");
+
+let count = 0;
+setInterval(function() {
+	meuEmissor.emit(nomeEvento, "no ok " + count++);
+}, 1000);
