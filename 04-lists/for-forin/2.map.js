86a50639f492eab09d5f28476bdc5e9576bb1cef
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-02-03 23:35:28 -0300

    usando o .map

--- 04-lists/for-forin/2.map.js
+++ 04-lists/for-forin/2.map.js
@@ -2,9 +2,12 @@ const service = require("./service");
 async function main() {
 	try {
 		const result = await service.obterPessoas("a");
-		const names = [];
-		result.results.forEach(function(item) {
-			names.push(item.name);
+		// const names = [];
+		// result.results.forEach(function(item) {
+		// 	names.push(item.name);
+		// });
+		const names = result.results.map(function(pessoa) {
+			return pessoa.name;
 		});
 		console.log("names", names);
 	} catch (err) {
