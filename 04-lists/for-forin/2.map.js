146d44737776143e5dfbef1f10f9ee9e0cd008ab
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-02-03 23:49:19 -0300

    criando um map com Array.prototype

--- 04-lists/for-forin/2.map.js
+++ 04-lists/for-forin/2.map.js
@@ -1,4 +1,13 @@
 const service = require("./service");
+
+Array.prototype.meuMap = function(callback) {
+	const novoArrayMapeado = [];
+	for (let indice = 0; indice <= this.length - 1; indice++) {
+		const resultado = callback(this[indice], indice);
+		novoArrayMapeado.push(resultado);
+	}
+	return novoArrayMapeado;
+};
 async function main() {
 	try {
 		const result = await service.obterPessoas("a");
@@ -9,7 +18,10 @@ async function main() {
 		// const names = result.results.map(function(pessoa) {
 		// 	return pessoa.name;
 		// });
-		const names = result.results.map(pessoa => pessoa.name);
+		// const names = result.results.map(pessoa => pessoa.name);
+		const names = result.results.meuMap(function(pessoa, indice) {
+			return `[${indice}] ${pessoa.name}`;
+		});
 		console.log("names", names);
 	} catch (err) {
 		console.error("Deu ruim ", error);
