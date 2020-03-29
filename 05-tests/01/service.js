9457a642a5029b88893e476c23fa823ee5cc142b
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-03-28 21:55:58 -0300

    usando nock para mocka os dados

--- 05-tests/01/service.js
+++ 05-tests/01/service.js
@@ -5,16 +5,18 @@ const URL = `https://swapi.co/api/people`;
 async function obterPessoas(nome) {
 	const url = `${URL}/?search=${nome}&format=json`;
 	const result = await get(url);
+	console.log(JSON.stringify(result.data));
+	// console.log(result.data);
 	return result.data.results.map(mapearPessoas);
 }
 
-function mapearPessoas (item){
+function mapearPessoas(item) {
 	return {
 		nome: item.name,
 		peso: item.height
-	}
+	};
 }
 
 module.exports = {
 	obterPessoas
-}
+};
--- 05-tests/01/test.js
+++ 05-tests/01/test.js
@@ -3,7 +3,50 @@ const { obterPessoas } = require("./service");
 
 // assert.ok(true);
 
+// instalamos o pacote nock para simalar requisições
+
+const nock = require("nock");
+
 describe("Star Wars Tests", function() {
+	this.beforeAll(() => {
+		const response = {
+			count: 1,
+			next: null,
+			previous: null,
+			results: [
+				{
+					name: "R2-D2",
+					height: "96",
+					mass: "32",
+					hair_color: "n/a",
+					skin_color: "white, blue",
+					eye_color: "red",
+					birth_year: "33BBY",
+					gender: "n/a",
+					homeworld: "https://swapi.co/api/planets/8/",
+					films: [
+						"https://swapi.co/api/films/2/",
+						"https://swapi.co/api/films/5/",
+						"https://swapi.co/api/films/4/",
+						"https://swapi.co/api/films/6/",
+						"https://swapi.co/api/films/3/",
+						"https://swapi.co/api/films/1/",
+						"https://swapi.co/api/films/7/"
+					],
+					species: ["https://swapi.co/api/species/2/"],
+					vehicles: [],
+					starships: [],
+					created: "2014-12-10T15:11:50.376000Z",
+					edited: "2014-12-20T21:17:50.311000Z",
+					url: "https://swapi.co/api/people/3/"
+				}
+			]
+		};
+		nock("https://swapi.co/api/people")
+			.get("/?search=r2-d2&format=json")
+			.reply(200, response);
+	});
+
 	it("deve buscar 0 R2D2 com o formato correto", async () => {
 		const expected = [{ nome: "R2-D2", peso: "96" }];
 		const nomeBase = `r2-d2`;
