4a04a47b4e9bdfdf4c9ba8e6629611c91220dfcc
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-03-29 12:19:51 -0300

    Manipulando arquivo - DELETE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -1,21 +1,21 @@
 const { deepEqual, ok } = require("assert");
-const database = require('./database')
+const database = require("./database");
 
 const DEFAULT_ITEM_CADASTRAR = { nome: "Flash", poder: "Speed", id: 1 };
 
 describe("Suite de manipulação de Herois", () => {
-	before( async () => {
-		await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
-	})
+	before(async () => {
+		await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+	});
 
 	it("deve pesquisar um heroi usando arquivo", async () => {
-		const expected = DEFAULT_ITEM_CADASTRAR
-		const [resultado] = await database.listar(expected.id)
+		const expected = DEFAULT_ITEM_CADASTRAR;
+		const [resultado] = await database.listar(expected.id);
 		// const posicaoUm = resultado[0]
 		// ok(resultado, expected)
 		// deepEqual(resultado, expected)
-		deepEqual(resultado, expected)
-	})
+		deepEqual(resultado, expected);
+	});
 
 	it("deve cadastrar um heroi usando arquivos", async () => {
 		const expected = DEFAULT_ITEM_CADASTRAR;
@@ -24,9 +24,15 @@ describe("Suite de manipulação de Herois", () => {
 		// 	id:2,
 		// 	nome: "Batman"
 		// };
-		const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
-		const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+		const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+		const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
 
 		deepEqual(actual, expected);
+	});
+
+	it("deve remover um heroi por id", async () => {
+		const expected = true;
+		const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+		deepEqual(resultado, expected);
 	});
 });
