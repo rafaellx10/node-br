2bcab8d184aae18e0d89444ef536613413cc023d
Rafael Leal Xavier <rafaellx10@gmail.com>
2020-04-03 00:03:30 -0300

    MONGODB - Listar Herois - READ

--- 09-muiti-db-postgres-mongodb/src/db/strategies/mongodb.js
+++ 09-muiti-db-postgres-mongodb/src/db/strategies/mongodb.js
@@ -60,6 +60,13 @@ class MongoDB extends ICrud {
 	create(item) {
 		return this._herois.create(item);
 	}
+
+	read(item, skip = 0, limit = 10) {
+		return this._herois
+			.find(item)
+			.skip(skip)
+			.limit(limit);
+	}
 }
 
 module.exports = MongoDB;
--- 09-muiti-db-postgres-mongodb/src/tests/mongodbStrategy.test.js
+++ 09-muiti-db-postgres-mongodb/src/tests/mongodbStrategy.test.js
@@ -8,9 +8,14 @@ const MOCK_HEROI_CADASTRAR = {
 	nome: "Mulher Maravilha",
 	poder: "Laço"
 };
+const MOCK_HEROI_DEFAULT = {
+	nome: `Homem Aranha-${Date.now()}`,
+	poder: "Teia"
+};
 describe("MongoDB Suite de testes", function() {
 	this.beforeAll(async () => {
 		await context.connect();
+		await context.create(MOCK_HEROI_DEFAULT);
 	});
 	it("Verificar conexão", async () => {
 		const result = await context.isConnected();
@@ -24,5 +29,26 @@ describe("MongoDB Suite de testes", function() {
 		// const result = await context.create(MOCK_HEROI_CADASTRAR);
 		// console.log("result ", result);
 		assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR);
+	});
+
+	it("Listar1", async () => {
+		const [{ nome, poder }] = await context.read({
+			nome: MOCK_HEROI_CADASTRAR.nome
+		});
+		const result = {
+			nome,
+			poder
+		};
+		assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+	});
+	it("Listar2", async () => {
+		const [{ nome, poder }] = await context.read({
+			nome: MOCK_HEROI_DEFAULT.nome
+		});
+		const result = {
+			nome,
+			poder
+		};
+		assert.deepEqual(result, MOCK_HEROI_DEFAULT);
 	});
 });
